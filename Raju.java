cf4c40b341f34b053a484f4e7bb9a135c24ca6fa
Ramya8179 <softwareramya61@gmail.com>
2025-05-31 08:41:29 +0530

    Raju completed the Creation of RDS & Load Balancer Servers

--- Raju.java
+++ Raju.java
@@ -3,5 +3,7 @@ class Raju
 	public static void main String[] args()
 	{
 		System.out.println("Raju can do Coding and Raju can Deploy the code to Servers");
+		System.out.println("Today my Task is to create RDS & Load Balancer Servers in AWS Cloud Platform);
+
 	}
 }
